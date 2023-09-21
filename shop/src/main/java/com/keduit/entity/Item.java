10016623b49f4403820816b20629659d7ad28304
SANGHYEON <ppsh0703@daum.net>
2023-09-21 11:05:57 +0900

    하이버네이트 jpa 연습중

---
+++ shop/src/main/java/com/keduit/entity/Item.java
@@ -0,0 +1,41 @@
+package com.keduit.entity;
+
+import com.keduit.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tbl_item")
+public class Item {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;  //상품 코드
+
+   @Column(length=50, nullable = false)
+   private String itemNm; //상품명
+
+   @Column(nullable = false)
+   private int price; // 가격
+
+   @Column(nullable = false)
+   private int stockNumber; // 재고수량
+
+   @Column(nullable = false)
+   @Lob
+   private String itemDetail; // 상품 상세 설명
+
+   @Enumerated(EnumType.STRING)
+   private ItemSellStatus itemSellStatus; //상품 판매 상태
+
+
+   private LocalDateTime regTime; // 등록 시간
+   private LocalDateTime updateTime; //수정시간
+
+}
